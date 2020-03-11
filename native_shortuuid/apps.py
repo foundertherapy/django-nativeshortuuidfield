6892f976900528a4018e6db23425138bbed3eebc
Laith Abu Zainih <laith.fawwaz@gmail.com>
2020-03-11 16:52:33 +0200

    init commit

---
+++ native_shortuuid/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NativeShortuuidConfig(AppConfig):
+    name = 'native_shortuuid'
---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+import setuptools
+from setuptools.command.install import install
+
+VERSION = "1.0"
+
+def readme():
+    """print long description"""
+    with open('README.rst') as f:
+        return f.read()
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+setuptools.setup(
+    name="django-nativeshortuuidfield",
+    version=VERSION,
+    description="A decoder/encoder Field for uuid",
+    long_description=readme(),
+    url="https://github.com/foundertherapy/django-nativeshortuuidfield",
+    author="Laith Abu Zainih",
+    author_email="systems@foundertherapy.co",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords='shortuuid uuid nativeshortuuid',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests==2.18.4",
+        "django>=1.11",
+        "shortuuid>=1.0"
+    ],
+    python_requires='>=3',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)
