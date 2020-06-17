5be1b0f11d8e23a31e5036a62d9cd62a1bc76da9
Salah Hadayda <hadayda@foundertherapy.co>
2020-06-17 17:25:20 +0300

    Add shortuuid validator

--- native_shortuuid/__init__.py
+++ native_shortuuid/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from .fields import NativeShortUUIDField
 from .fields import NativeShortUUIDFormField
+from . import validation
 
 try:
     VERSION = __import__('pkg_resources') \
