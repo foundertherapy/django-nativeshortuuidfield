8da866db3fda051b60312d51706977c09f2515d9
hadayda <hadayda@foundertherapy.co>
2022-05-19 17:23:19 +0300

    Fix search mixin & add Serializer Field

--- native_shortuuid/admin.py
+++ native_shortuuid/admin.py
@@ -1,7 +1,18 @@
+import django.db.models
+
 import native_shortuuid
 
 
 class NativeUUIDSearchMixin:
+    def is_valid_shortuuid(self, seach_term):
+        try:
+            # It's not decode-able, thus, not a shortuuid
+            native_shortuuid.decode(seach_term)
+        except ValueError:
+            return False
+
+        return True
+
     def get_search_fields(self, request):
         search_fields = super().get_search_fields(request)
         if not hasattr(self, '_uuid_search_fields'):
@@ -10,15 +21,13 @@ class NativeUUIDSearchMixin:
         return self._non_uuid_search_fields or self._non_uuid_search_fields + ('id', )
 
     def get_search_results(self, request, queryset, search_term):
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        try:
-            search_term_as_uuid = native_shortuuid.decode(search_term.strip())
-        except ValueError:
-            pass
-        else:
-            for uuid_search_field in self._uuid_search_fields:
-                search_params = {
-                    uuid_search_field: search_term_as_uuid,
-                }
-                queryset |= self.model.objects.filter(**search_params)
-        return queryset, use_distinct
+        # We send the search_term as None to remove the search_fields from the query and add them properly here
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term=None)
+        field_queries = django.db.models.Q()
+        if self.is_valid_shortuuid(search_term):
+            for field_name_uuid in self._uuid_search_fields:
+                field_queries |= django.db.models.Q(**{field_name_uuid: search_term, })
+
+        for field_name in self._non_uuid_search_fields:
+            field_queries |= django.db.models.Q(**{'{}__icontains'.format(field_name): search_term, })
+        return queryset.filter(field_queries), use_distinct
